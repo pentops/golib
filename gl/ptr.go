1b5307b3bfe0133ebfc937873944f300ec722978
Damien Whitten <dw@chaos404.com>
2025-01-06 17:22:16 -0800

    Unwrap

--- gl/ptr.go
+++ gl/ptr.go
@@ -12,3 +12,24 @@ func Coalesce[T any](fallback T, try ...*T) T {
 	}
 	return fallback
 }
+
+func Opt[T any](v *T) (o T) {
+	if v == nil {
+		return // the default value, "", 0, false etc
+	}
+	return *v
+}
+
+func MustUnwrap[T any](v *T) T {
+	if v == nil {
+		panic("nil value")
+	}
+	return *v
+}
+
+func Must[T any](v T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
